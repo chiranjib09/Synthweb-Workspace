06b1c434d6c920f25fe883d3a8dcb6b09e7ddd1a
chiranjib09 <chiranjibdas28094@gmail.com>
2024-08-13 12:48:37 +0530

    new file

---
+++ Functions in JS/FunctionArrow.js
@@ -0,0 +1,7 @@
+// It provide a shorter syntax to define functions and useful for writing concise one-liners or for use in callbacks.
+
+
+const multiply = (a, b) => a * b;
+
+
+console.log(multiply(4, 7));
