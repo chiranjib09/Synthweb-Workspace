c4d4a844db6b552d482ed2125fc345353a34c5c8
chiranjib09 <chiranjibdas28094@gmail.com>
2024-08-13 13:02:34 +0530

    changes

--- Functions in JS/FunctionArrow.js
+++ Functions in JS/FunctionArrow.js
@@ -1,7 +1,5 @@
-// It provide a shorter syntax to define functions and useful for writing concise one-liners or for use in callbacks.
+// It provide a shorter syntax to define functions and useful                                                           for writing concise one-liners or for use in callbacks.
 
 
 const multiply = (a, b) => a * b;
-
-
 console.log(multiply(4, 7));
