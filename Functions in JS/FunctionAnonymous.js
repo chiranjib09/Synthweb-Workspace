23bbcf9d8a6ec3f8ee03c286d478c9c83863f81d
chiranjib09 <chiranjibdas28094@gmail.com>
2024-08-13 15:02:06 +0530

    new code

---
+++ Functions in JS/FunctionAnonymous.js
@@ -0,0 +1,6 @@
+// It is used as an argument to other functions, such as in event handlers or callbacks.
+
+
+setTimeout(function() {
+  console.log("This message is displayed after 2 seconds.");
+}, 2000);
