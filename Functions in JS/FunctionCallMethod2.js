5bc2452239deaa15afa8241ec919c121c6ea97c1
chiranjib09 <chiranjibdas28094@gmail.com>
2024-08-14 14:57:34 +0530

    new file added again

---
+++ Functions in JS/FunctionCallMethod2.js
@@ -0,0 +1,9 @@
+//Parsing Arguments
+//process of extracting and interpreting values passed to a function or a script.
+
+
+function sum (a,b){
+
+}
+const result = sum.call(null,3,4);
+console.log(result);
