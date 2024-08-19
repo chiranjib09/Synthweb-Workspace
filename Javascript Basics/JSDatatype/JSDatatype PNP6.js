2728429078dfb8b26187ff68a88b7477ce18bc37
chiranjib09 <chiranjibdas28094@gmail.com>
2024-08-19 13:08:08 +0530

    modified with files and folders

---
+++ Javascript Basics/JSDatatype/JSDatatype PNP6.js
@@ -0,0 +1,11 @@
+//Primitive and non primitive Datatype
+//Array DAtatype
+
+let a = new Array();
+let b = new Array(10);
+let d = new Array(1,2,3, "Hello");
+console.log("Value of a= " + a);
+console.log("Value of b= " + b);
+console.log("Value of d= " + d);
+
+
