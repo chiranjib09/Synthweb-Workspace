b0f205900378651942305692f6254a213820f035
chiranjib09 <chiranjibdas28094@gmail.com>
2024-08-19 17:04:03 +0530

    new operators file added

---
+++ Javascript Basics/JS Operators/ArithmeticOperators.js
@@ -0,0 +1,10 @@
+// Arithmetic Operators are used to perform mathematical operations on numbers.
+
+let a = 50;
+let b = 20;
+
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
