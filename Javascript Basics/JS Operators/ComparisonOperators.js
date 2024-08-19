5bd28ea2eb55a8402de1e6ec0eb0c6230ad4bddc
chiranjib09 <chiranjibdas28094@gmail.com>
2024-08-19 17:04:47 +0530

    new operators file added

---
+++ Javascript Basics/JS Operators/ComparisonOperators.js
@@ -0,0 +1,6 @@
+// Comparison Operators are used to compare two values and return a Boolean (true or false)
+
+console.log (5 == '5');
+console.log (10 === '100');
+console.log (4 != '9');
+console.log (5 !== '5');
