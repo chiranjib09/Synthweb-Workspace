56d8e8c188d12cd54ec35845056b13fd19009d95
chiranjib09 <chiranjibdas28094@gmail.com>
2024-08-19 15:30:54 +0530

    variables

---
+++ Javascript Basics/JSVariables/const.js
@@ -0,0 +1,10 @@
+//JavaScript constants are the variables whose values remain unchanged throughout the execution of the program.                  You can declare constants using the const keyword.
+
+const cars = ["Maruti", "Jaguar", "Honda"];
+cars[0] = "Maruti";
+cars.push("BMW");
+console.log(cars);
+
+
+
+//Use const when you declare:                                                                                                                 A new Array                                                                                                                                   A new Object                                                                                                                                  A new Function                                                                                                                                A new RegExp
