180559c4ffcfe1771cc58d356d716e30c1a8a6bb
chiranjib09 <chiranjibdas28094@gmail.com>
2024-08-19 15:31:15 +0530

    variables

---
+++ Javascript Basics/JSVariables/Let.js
@@ -0,0 +1,13 @@
+// Let keyword
+//The let keyword in JavaScript is used to declare variables that are block-scoped,                                             which means they are only accessible within the block {} where they are defined
+
+
+let y = 30;
+y = 40;
+console.log(y)
+
+
+
+
+
+//Variables declared with let have Block Scope                                                                            Variables declared with let must be Declared before use                                                                   Variables declared with let cannot be Redeclared in the same scope
