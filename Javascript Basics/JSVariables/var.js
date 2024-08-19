dd06e3bd572947ab295b01decfe2280052e05a98
chiranjib09 <chiranjibdas28094@gmail.com>
2024-08-19 15:31:38 +0530

    variables

---
+++ Javascript Basics/JSVariables/var.js
@@ -0,0 +1,8 @@
+// Var Keywords
+//When you declare a variable with var, the variable can have a function scope or                                                a global scope if not declared inside a function.
+
+
+var x = 10;
+x = 20;
+console.log(x);
+
