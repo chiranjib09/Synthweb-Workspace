9fffbc4570ce857e9722e3d7ae29c2ba3c073bb2
chiranjib09 <chiranjibdas28094@gmail.com>
2024-08-19 11:57:35 +0530

    added new js files

---
+++ Javascript Basics/JSDatatype PNP2.js
@@ -0,0 +1,8 @@
+// Primitive Datatype and non primitive datatype
+//String
+
+let str = "Hello All";
+let str1 = "Welcome to my channel";
+console.log("value of str=" + str);
+console.log ("value of srt1=" + str1);
+
