3a3610156ac17c1f59a091b493298bb3549a6721
chiranjib09 <chiranjibdas28094@gmail.com>
2024-08-19 11:58:40 +0530

    added new js files

---
+++ Javascript Basics/JSDatatype PNP5.js
@@ -0,0 +1,10 @@
+// Primitive and non primitive Datatype
+//Object Datatype
+
+let person = {
+  firstName: "Luiza",
+  lastName: "Sahil",
+};
+
+console.log(person.firstName
+  + "  " + person.lastName);
