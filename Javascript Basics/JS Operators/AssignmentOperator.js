f5a109cf3a17de528dd76e28732569cdb89b1b92
chiranjib09 <chiranjibdas28094@gmail.com>
2024-08-20 14:29:56 +0530

    new operators file added

---
+++ Javascript Basics/JS Operators/AssignmentOperator.js
@@ -0,0 +1,12 @@
+//Assignment operators in JavaScript are used to assign values to variables. The most common assignment operator is the simple assignment (=), but there are several compound assignment operators that combine an arithmetic operation with assignment.
+
+const obj = {};
+obj.x = 3;
+console.log(obj.x);
+console.log(obj);
+
+const key = "y";
+obj[key] = 5;
+console.log(obj[key]);
+console.log(obj);
+
