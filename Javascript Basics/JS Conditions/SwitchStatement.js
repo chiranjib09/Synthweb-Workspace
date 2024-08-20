7aac52948bab6e495246b5e939c8ddc6f5252a54
chiranjib09 <chiranjibdas28094@gmail.com>
2024-08-20 16:09:31 +0530

    Added new condition file

---
+++ Javascript Basics/JS Conditions/SwitchStatement.js
@@ -0,0 +1,34 @@
+//The switch statement is used to perform different actions based on different conditions.
+//Use the switch statement to select one of many code blocks to be executed.
+
+let day = 3;
+let dayName;
+
+switch (day) {
+  case 1:
+    dayName = "Monday";
+    break;
+  case 2:
+    dayName = "Tuesday";
+    break;
+  case 3:
+    dayName = "Wednesday";
+    break;
+  case 4:
+    dayName = "Thursday";
+    break;
+  case 5:
+    dayName = "Friday";
+    break;
+  case 6:
+    dayName = "Saturday";
+    break;
+  case 7:
+    dayName = "Sunday";
+    break;
+  default:
+    dayName = "Invalid day";
+}
+
+console.log(dayName);
+
