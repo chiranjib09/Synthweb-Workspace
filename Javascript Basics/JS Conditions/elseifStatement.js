c9edf1ac9215b0f1834c18ffe8707259ad3e00a5
chiranjib09 <chiranjibdas28094@gmail.com>
2024-08-20 15:40:34 +0530

    Condition files added

---
+++ Javascript Basics/JS Conditions/elseifStatement.js
@@ -0,0 +1,15 @@
+//The else if statement allows you to specify a new condition to test if the first condition is false. You can chain multiple else if statements to handle different cases.
+
+let score = 85;
+
+if (score >=90){
+  console.log("Grade:A");
+} else if (score >=80){
+  console.log("Grade:B");
+} else if (score >=70){
+  console.log("Grade:C");
+} else if (score >=60){
+  console.log("Grade:D");
+} else if (score >=50){
+  console.log("Grade:E");
+}
