c61c3ad94464ec14ab028d1c7085f8352e55278c
chiranjib09 <chiranjibdas28094@gmail.com>
2024-08-20 15:40:59 +0530

    Condition files added

---
+++ Javascript Basics/JS Conditions/elseStatement.js
@@ -0,0 +1,10 @@
+//The else statement specifies a block of code to be executed if the condition in the if statement is false.
+
+let age = 16;
+
+if (age >= 18) {
+  console.log("You are eligible to vote.");
+} else {
+  console.log("You are not eligible to vote yet.");
+};
+
