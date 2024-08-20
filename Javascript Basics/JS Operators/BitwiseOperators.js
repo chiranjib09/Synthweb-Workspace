75051dfa8be236b97c923a1a052c9ac383c67b49
chiranjib09 <chiranjibdas28094@gmail.com>
2024-08-20 13:12:48 +0530

    operators files added

---
+++ Javascript Basics/JS Operators/BitwiseOperators.js
@@ -0,0 +1,19 @@
+//The JavaScript bitwise operators are used to perform bit-level operations on integer
+
+let a = 1;
+let b = 2;
+
+let result = a | b ;
+let result1 = a & b ;
+let result2 = a ^ b ;
+let result3 = ~a;
+let result4 = a << 1 ;
+let result5 = a >> 1 ;
+console.log(result);
+console.log(result1);
+console.log(result2);
+console.log(result3);
+console.log(result4);
+console.log(result5);
+
+
---
+++ Javascript Basics/JS Operators/TypeOperators1.js
@@ -0,0 +1,12 @@
+//n JavaScript, type operators are used to determine or manipulate the type of a variable or an expression. The most commonly used type operators in JavaScript are typeof, instanceof, and the newer in operator.
+
+//The typeof operator returns a string indicating the type of the operand. It's used to determine the data type of a variable or expression.
+
+console.log(typeof 45);
+console.log(typeof "Hello");
+console.log(typeof true);
+console.log(typeof undefined);
+console.log(typeof {name: 'Jhon'});
+console.log(typeof [1,2,3]);
+console.log(typeof null);
+
