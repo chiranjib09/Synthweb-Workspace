e5098d068eb6e0b070df1b6db73ed8233ff9b0ee
chiranjib09 <chiranjibdas28094@gmail.com>
2024-08-20 15:39:40 +0530

    Condition files added

---
+++ Javascript Basics/JS Conditions/ifStatement.js
@@ -0,0 +1,8 @@
+//Use the if statement to specify a block of JavaScript code to be executed if a condition is true.
+
+let age = 18;
+
+if (age >= 18) {
+  console.log("You are eligible to work for this job.");
+};
+
