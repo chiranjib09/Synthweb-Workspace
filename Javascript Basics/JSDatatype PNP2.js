2b77646e9c9b9abfd9711506986434b4fefd319e
CHIRANJIB DAS <103299833+chiranjib09@users.noreply.github.com>
2024-08-21 11:22:20 +0530

    Delete Javascript Basics/JSDatatype PNP2.js

--- Javascript Basics/JSDatatype PNP2.js
+++
@@ -1,8 +0,0 @@
-// Primitive Datatype and non primitive datatype
-//String
-
-let str = "Hello All";
-let str1 = "Welcome to my channel";
-console.log("value of str=" + str);
-console.log ("value of srt1=" + str1);
-
