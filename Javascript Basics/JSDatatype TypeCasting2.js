8d354fbefef9ed2400dcf424e0487e8e3f5d75fa
CHIRANJIB DAS <103299833+chiranjib09@users.noreply.github.com>
2024-08-21 11:23:27 +0530

    Delete Javascript Basics/JSDatatype TypeCasting2.js

--- Javascript Basics/JSDatatype TypeCasting2.js
+++
@@ -1,8 +0,0 @@
-//Typecasting in Strings
-
-input = 25
-console.log("Inout value is:" , input,typeof(input));
-console.log("Arothmetic operation before typecasting with its type is:", (10+input), typeof((10 + input)));
-sInput = String(input);
-console.log ("After the type casting is done the type is:",sInput,typeof(sInput));
-console.log("Arithmetic operation after typecasting with its type is:", (10 + sInput), typeof(10 + sInput));
