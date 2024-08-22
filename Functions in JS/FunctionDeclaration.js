5ca0f0a822009350a1e025dac97d0c0a41485601
chiranjib09 <chiranjibdas28094@gmail.com>
2024-08-22 16:50:20 +0530

    Added Functionparameter file

---
+++ Functions in JS/FunctionDeclaration.js
@@ -0,0 +1,12 @@
+//function declaration is a way to define a function using the function keyword, followed by the function's name, a list of parameters in parentheses, and the function body enclosed in curly braces {}.
+
+//Function declaration with multiple Parameters
+function addNumbers(a, b) {
+  return a + b;
+}
+
+const sum1 = addNumbers(3, 4);
+const sum2 = addNumbers(10, 20);
+
+console.log(sum1);
+console.log(sum2);
