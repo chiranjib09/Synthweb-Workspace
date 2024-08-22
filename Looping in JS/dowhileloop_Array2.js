de26a4844aedf320838a8b7a29cdb24f37e2baa7
chiranjib09 <chiranjibdas28094@gmail.com>
2024-08-22 15:59:51 +0530

    Descritive comments added to the file

--- Looping in JS/dowhileloop_Array2.js
+++ Looping in JS/dowhileloop_Array2.js
@@ -5,3 +5,5 @@ do {
   console.log(fruits[i]);
   i++;
 } while (i < fruits.length);
+
+//The do-while loop is particularly useful when you need to process elements in an array at least once, even if the array is empty.
