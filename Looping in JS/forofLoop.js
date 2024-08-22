97e285751b7e7af0e5df200f103101a913cb1196
chiranjib09 <chiranjibdas28094@gmail.com>
2024-08-22 16:11:08 +0530

    Descritive comments added to the file

--- Looping in JS/forofLoop.js
+++ Looping in JS/forofLoop.js
@@ -3,3 +3,6 @@ const array = [ 1, 2, 3, 4, 5 ];
 for (const item of array) {
     console.log(item);
 }
+
+//The for...of loop is a more concise and readable way to iterate over iterable objects, such as arrays and strings.
+//It provides a simpler syntax compared to traditional loops like for or for...in.
