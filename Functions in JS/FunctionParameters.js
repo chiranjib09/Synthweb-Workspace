f9208c2f046645618ccfede7e90fb38bd3ca604f
chiranjib09 <chiranjibdas28094@gmail.com>
2024-08-22 16:53:24 +0530

    FunctionParameter file added

---
+++ Functions in JS/FunctionParameters.js
@@ -0,0 +1,11 @@
+// function parameters are the variables that you define in the function declaration to accept values when the function is called. These values are called arguments when you pass them to the function.
+
+
+function multiply(a, b) {
+  return a * b;
+}
+const result1 = multiply(5, 3);
+const result2 = multiply(10, 2);
+
+console.log(result1);
+console.log(result2);
