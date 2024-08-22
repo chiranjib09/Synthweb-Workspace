c861faf2fa1698b1d9c05de0e9e2975e99d04e4c
chiranjib09 <chiranjibdas28094@gmail.com>
2024-08-22 15:02:14 +0530

    Breakstatement5 syntax added

---
+++ Looping in JS/Breakstatement5.js
@@ -0,0 +1,14 @@
+// Break Statement using switch statement loop
+
+for (let i = 0; i < 5; i++) {
+  switch (i) {
+    case 2:
+      console.log("Two");
+      break;
+    case 3:
+      console.log("Three");
+      break;
+    default:
+      console.log(i);
+  }
+}
