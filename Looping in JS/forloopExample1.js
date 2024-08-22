f98dbcce9c5e49995e23f36ddd0ee5d7f0efe209
chiranjib09 <chiranjibdas28094@gmail.com>
2024-08-22 16:10:32 +0530

    Descritive comments added to the file

--- Looping in JS/forloopExample1.js
+++ Looping in JS/forloopExample1.js
@@ -1,3 +1,5 @@
 for (let i = 0; i <5; i++) {
   console.log(i);
-}
+};
+
+//The for loop is a control flow statement used to execute a block of code repeatedly for a specified number of times or until a certain condition is met.
