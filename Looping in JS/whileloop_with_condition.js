cb0b254d4e2739797993e5f9166210e476c4c8a9
chiranjib09 <chiranjibdas28094@gmail.com>
2024-08-22 16:12:59 +0530

    Descritive comments added to the file

--- Looping in JS/whileloop_with_condition.js
+++ Looping in JS/whileloop_with_condition.js
@@ -7,3 +7,8 @@ while (randomNum <= 0.8) {
 }
 
 console.log("Finished, random number is greater than 0.8:", randomNum);
+
+
+//The while loop evaluates the condition before each iteration.
+//If the condition is true, the code block within the loop is executed.
+// The loop repeats as long as the condition remains true.
