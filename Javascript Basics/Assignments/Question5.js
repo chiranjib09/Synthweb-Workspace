63e7868a7e86fb8307c5950a63a590efd528a047
chiranjib09 <chiranjibdas28094@gmail.com>
2024-08-22 11:16:52 +0530

    minor edits

--- Javascript Basics/Assignments/Question5.js
+++ Javascript Basics/Assignments/Question5.js
@@ -6,8 +6,7 @@ function countOccurrences(arr, value) {
   for (let i = 0; i < arr.length; i++) {
       if (arr[i] === value) {
           count++;
-      }
-  }
+      }}
   return count;
 }
 const numbers = [1, 2, 3, 4, 3, 2, 3, 5, 3];
