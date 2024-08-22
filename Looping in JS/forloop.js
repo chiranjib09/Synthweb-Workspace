2e2e37bfd5296fe1df9a372a64a7645575f020c9
chiranjib09 <chiranjibdas28094@gmail.com>
2024-08-22 16:09:37 +0530

    Descritive comments added to the file

--- Looping in JS/forloop.js
+++ Looping in JS/forloop.js
@@ -3,3 +3,5 @@ let fruits = ["Apple", "Banana", "Mango", "Orange"];
 for (let i = 0; i < fruits.length; i++) {
     console.log(fruits[i]);
 }
+
+//The for loop is a control flow statement used to execute a block of code repeatedly for a specified number of times or until a certain condition is met.
