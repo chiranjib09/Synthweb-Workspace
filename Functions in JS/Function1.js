5f73cb5a6c3d2037960dedc45675297b07e009bd
chiranjib09 <chiranjibdas28094@gmail.com>
2024-08-22 11:13:07 +0530

    minor changes

--- Functions in JS/Function1.js
+++ Functions in JS/Function1.js
@@ -1,7 +1,9 @@
-//function declaration of a name 
+//function declaration of a name
 
 function greet(name) {
   return `My Name Is, ${name}!`;
 }
 
 console.log(greet("Chiranjib"));
+
+//write a function to add two number
