20b1718651e9f35bff74e4c7367a9e5a08908851
chiranjib09 <chiranjibdas28094@gmail.com>
2024-08-22 14:59:48 +0530

    Breakstatement1 syntax added

---
+++ Looping in JS/Breakstatement1.js
@@ -0,0 +1,11 @@
+//It was used to "jump out" of a switch() statement.
+//The break statement can also be used to jump out of a loop .
+
+//Break statement using FOR loop
+
+for (let i = 0; i < 10; i++) {
+  if (i === 5) {
+    break;
+  }
+  console.log(i);
+}
