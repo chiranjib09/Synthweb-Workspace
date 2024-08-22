47124e2f310b5f2c37d0255ed7660024ee42155e
chiranjib09 <chiranjibdas28094@gmail.com>
2024-08-22 15:59:06 +0530

    Descritive comments added to the file

--- Looping in JS/dowhileloop.js
+++ Looping in JS/dowhileloop.js
@@ -4,3 +4,7 @@ do {
   console.log(count);
   count++;
 } while (count <= 5);
+
+
+//The do-while loop is a control flow statement that executes a block of code at least once, and then repeatedly as long as a specified condition remains true.
+//It's particularly useful when you need to ensure that a block of code is executed at least once, regardless of the initial condition.
