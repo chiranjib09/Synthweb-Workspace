97f52050f17754b2d825ebc313862ecba85f534c
chiranjib09 <chiranjibdas28094@gmail.com>
2024-08-27 12:31:33 +0530

    Block scope file added

---
+++ Javascript Basics/Scope and Closure/Block.js
@@ -0,0 +1,19 @@
+//Block Scope under LOCAL SCOPE
+
+
+function display_scopes() {
+  // declare variable in local scope
+  let message = "local";
+
+  if (true) {
+
+      // declare block-level variable
+      let message = "block-level";
+
+      console.log(`inner scope: ${message}`);
+  }
+
+  console.log(`outer scope: ${message}`);
+}
+
+display_scopes();
