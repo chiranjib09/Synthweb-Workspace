f2ae8908ac984dbcbb9daf66fdf3b091f7441e57
chiranjib09 <chiranjibdas28094@gmail.com>
2024-08-27 10:30:22 +0530

    Anonymous function 1 file added

---
+++ Javascript Basics/Function Expression/AnonymousFunction1.js
@@ -0,0 +1,7 @@
+//An anonymous function is simply a function that does not have a name. Unlike named functions, which are declared with a name for easy reference, anonymous functions are usually created for specific tasks and are often assigned to variables or used as arguments for other functions.
+
+let sum = function(a, b) {
+  return a + b;
+};
+
+console.log(sum(5, 3));
