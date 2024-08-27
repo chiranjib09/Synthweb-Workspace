368fd2677fc0fab1c1c22e38070a0ae8285b4257
chiranjib09 <chiranjibdas28094@gmail.com>
2024-08-27 10:32:40 +0530

    Arrow function 2 file added

---
+++ Javascript Basics/Function Expression/ArrowFunction2.js
@@ -0,0 +1,8 @@
+//Arrow Function with IMPLICIT RETURN
+
+//If the function body has only a single expression, you can write it in a more concise form.
+//The function body is replaced with the expression, and the return statement is implicit.
+
+const multiply = (a, b) => a * b;
+
+console.log(multiply(4, 3));
