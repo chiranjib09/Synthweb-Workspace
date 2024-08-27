6053b66effbf2120be611ca3643bf7bbfa20a513
chiranjib09 <chiranjibdas28094@gmail.com>
2024-08-27 12:30:20 +0530

    Global scope file added

---
+++ Javascript Basics/Scope and Closure/Global.js
@@ -0,0 +1,8 @@
+let globalVar = "When a variable is in the global scope, it's called a global variable.";
+
+function displayGlobalVar() {
+    console.log(globalVar);
+}
+
+displayGlobalVar();
+console.log(globalVar);
