b0538a6b711b6a1457f0a3bc52770ea1f8806d88
chiranjib09 <chiranjibdas28094@gmail.com>
2024-08-27 12:30:51 +0530

    local scope file added

---
+++ Javascript Basics/Scope and Closure/Local.js
@@ -0,0 +1,10 @@
+//Function Scope under Local Scope
+function localFunction() {
+  let localVar = "I'm a local variable";
+  console.log(localVar); 
+  // Outputs: I'm a local variable
+}
+
+localFunction();
+console.log(localVar); 
+// output Reference Error: localVar is not defined
