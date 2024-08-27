d07e04964278768a28418421c40b5ad337daa008
chiranjib09 <chiranjibdas28094@gmail.com>
2024-08-27 10:30:44 +0530

    Anonymous function 2 file added

---
+++ Javascript Basics/Function Expression/AnonymousFunction2.js
@@ -0,0 +1,11 @@
+//Anonymous Function as a Callback
+//Anonymous functions are often used as callbacks, especially with array methods like map, filter, and forEach.
+//They are useful when you need to perform a simple operation and don't want to declare a named
+
+let numbers = [1, 2, 3, 4, 5];
+
+let doubled = numbers.map(function(num) {
+    return num * 2;
+});
+
+console.log(doubled);
