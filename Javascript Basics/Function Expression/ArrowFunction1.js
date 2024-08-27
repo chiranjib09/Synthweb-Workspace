147e9db82a25f8b6fe7bfc4a0e243dcbd0229685
chiranjib09 <chiranjibdas28094@gmail.com>
2024-08-27 10:32:20 +0530

    Arrow function 1 file added

---
+++ Javascript Basics/Function Expression/ArrowFunction1.js
@@ -0,0 +1,10 @@
+//ARROW FUNCTION
+//In an Arrow function, everything remains the same, except here we don’t need the function keyword also. Here, we define the function by a single parenthesis and then ‘=>’ followed by the function body.
+
+//Arrow Function with MULTIPLE PARAMETERS
+
+const sum = (a, b) => {
+  return a + b;
+};
+
+console.log(sum(5, 3));
