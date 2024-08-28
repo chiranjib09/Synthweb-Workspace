94e8afb9603ae2fd29a3a7ac08eb413345ada910
chiranjib09 <chiranjibdas28094@gmail.com>
2024-08-28 17:17:43 +0530

    Practice Question 3 added

---
+++ Array in JS/Question3.js
@@ -0,0 +1,8 @@
+//Given an array of words ['hello', 'world', 'how', 'are', 'you'], join these words into a single string with each word separated by a space in javascript
+
+
+let words = ['hello', 'world', 'how', 'are', 'you'];
+
+let sentence = words.join(' ');
+
+console.log(sentence);
