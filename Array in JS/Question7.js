654143610cd2291a792b521b9d51283bbee0fbbe
chiranjib09 <chiranjibdas28094@gmail.com>
2024-08-29 10:46:06 +0530

    Practice Question 7  added

---
+++ Array in JS/Question7.js
@@ -0,0 +1,8 @@
+//Given the array let colors = ["red", "green", "blue", "yellow"];, destructure it into individual variables. Then, print the first two colors in javascript
+
+let colors = ["red", "green", "blue", "yellow"];
+
+let [firstColor, secondColor, thirdColor, fourthColor] = colors;
+
+console.log(firstColor);
+console.log(secondColor);
