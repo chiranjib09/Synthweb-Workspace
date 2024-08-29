c93b8564e75afd933d54ab0d96aece806a740fe2
chiranjib09 <chiranjibdas28094@gmail.com>
2024-08-29 16:26:54 +0530

    minor changes

---
+++ Javascript Basics/JS String/Slice(start,end).js
@@ -0,0 +1,6 @@
+//extracts a part of the string and returns it as a new string. The end parameter is optional and non-inclusive.
+
+
+let str = "Hello, World!";
+let slicedStr = str.slice(7, 12);
+console.log(slicedStr);
