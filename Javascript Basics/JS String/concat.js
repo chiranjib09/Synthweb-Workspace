1ee273bba893a5d16e4d936856b1883bd9caeda2
chiranjib09 <chiranjibdas28094@gmail.com>
2024-08-29 16:03:19 +0530

    concat  method file added

---
+++ Javascript Basics/JS String/concat.js
@@ -0,0 +1,6 @@
+//Concatenates the string arguments to the calling string and returns a new string.
+
+let str1 = "Hello";
+let str2 = "World";
+let result = str1.concat(", ", str2, "!");
+console.log(result);
