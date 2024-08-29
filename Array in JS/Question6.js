8d8388fe22b9967495a0519f33b8ae4c791f1776
chiranjib09 <chiranjibdas28094@gmail.com>
2024-08-29 10:45:35 +0530

    Practice Question 6 added

---
+++ Array in JS/Question6.js
@@ -0,0 +1,10 @@
+//Create a 2x3 matrix using a multidimensional array. Write code to print the element at the second row and third column of the matrix in javascript
+
+
+let matrix = [
+  [1, 2, 3],
+  [4, 5, 6]
+];
+
+let element = matrix[1][2];
+console.log(element);
