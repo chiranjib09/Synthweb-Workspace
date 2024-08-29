75e736eb0509cff88bd672b1679000ef46ffc417
chiranjib09 <chiranjibdas28094@gmail.com>
2024-08-29 12:00:26 +0530

    IndexOfsubstring Method  file added

---
+++ Javascript Basics/JS String/IndexOfsubstring.js
@@ -0,0 +1,6 @@
+//Returns the index of the first occurrence of the specified substring. Returns -1 if not found.
+
+
+let str = "Hello, World!";
+let index = str.indexOf("World");
+console.log(index);
