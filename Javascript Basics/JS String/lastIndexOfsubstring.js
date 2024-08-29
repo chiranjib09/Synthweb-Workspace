4f2c5d8b806f2b57e53e0072682ac324f6113386
chiranjib09 <chiranjibdas28094@gmail.com>
2024-08-29 12:01:40 +0530

    lastIndexOfsubstring Method  file added

---
+++ Javascript Basics/JS String/lastIndexOfsubstring.js
@@ -0,0 +1,5 @@
+//Returns the index of the last occurrence of the specified substring. Returns -1 if not found.
+
+let str = "Hello, World! Hello again!";
+let index = str.lastIndexOf("Hello");
+console.log(index);
