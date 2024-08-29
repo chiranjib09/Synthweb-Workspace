8644a0048c1ed92981f3501fc485805177c58659
chiranjib09 <chiranjibdas28094@gmail.com>
2024-08-29 16:21:25 +0530

    repeat  method file added

---
+++ Javascript Basics/JS String/repeat.js
@@ -0,0 +1,5 @@
+//Returns a new string containing the specified number of copies of the string on which it was called.
+
+let str = "Hello";
+let repeatedStr = str.repeat(3);
+console.log(repeatedStr);
