95cb03e7d6566e379a0ea1a295f01c4a8bad0be9
chiranjib09 <chiranjibdas28094@gmail.com>
2024-08-29 16:23:57 +0530

    Substr(start, length) 0  method file added

---
+++ Javascript Basics/JS String/Substr(start,length).js
@@ -0,0 +1,5 @@
+//Extracts a part of the string starting at the specified index and continuing for the specified number of characters.
+
+let str = "Hello, World!";
+let substr = str.substr(7, 5);
+console.log(substr);
