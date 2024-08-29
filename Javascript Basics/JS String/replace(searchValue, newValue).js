f5d6e2f7a2bfa8e967ba386d87fb4ecb04c6154a
chiranjib09 <chiranjibdas28094@gmail.com>
2024-08-29 16:27:30 +0530

    minor changes

---
+++ Javascript Basics/JS String/replace(searchValue, newValue).js
@@ -0,0 +1,5 @@
+//Replaces the first occurrence of the specified substring with a new substring.
+
+let str = "Hello, World!";
+let newStr = str.replace("World", "JavaScript");
+console.log(newStr); // Output: "Hello, JavaScript!"
