3ef44d5e2b6c29d49bb965b8ecdc31d1a6f1b867
chiranjib09 <chiranjibdas28094@gmail.com>
2024-08-30 16:26:17 +0530

    BooleanValues file added

---
+++ Javascript Basics/Boolean DataType/BooleanValues.js
@@ -0,0 +1,7 @@
+//Boolean values in JavaScript are either true or false. You can assign them directly, or they can be the result of a comparison or expression.
+
+let isTrue = true;
+let isFalse = false;
+
+console.log(isTrue);
+console.log(isFalse);
