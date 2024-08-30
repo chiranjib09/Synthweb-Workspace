90ba2b169753b7614a27756298e732fd670c75c9
chiranjib09 <chiranjibdas28094@gmail.com>
2024-08-30 15:38:17 +0530

    Date 6 file added

---
+++ Javascript Basics/JS Date Object/Date6.js
@@ -0,0 +1,10 @@
+//Set Date Components
+
+//You can modify the components of a Date object using setter methods.
+
+let setDate = new Date();
+setDate.setFullYear(2025);
+setDate.setMonth(11);
+setDate.setDate(25);
+
+console.log(setDate);
