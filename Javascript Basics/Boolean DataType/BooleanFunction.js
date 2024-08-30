6fee16782e4b4f5b90f1aa9abb85b02fea9eff08
chiranjib09 <chiranjibdas28094@gmail.com>
2024-08-30 16:25:05 +0530

    BooleanFunction file added

---
+++ Javascript Basics/Boolean DataType/BooleanFunction.js
@@ -0,0 +1,15 @@
+//JavaScript provides the Boolean() function to convert other data types into a Boolean value.
+
+//Truthy and Falsy Values:
+//In JavaScript, all values are inherently either "truthy" or "falsy."
+
+//Falsy values: Values that are considered false when converted to Boolean.
+
+//false, 0, "" (empty string), null, undefined, NaN
+//Truthy values: All other values, including non-empty strings, non-zero numbers, objects, and arrays.
+
+
+console.log(Boolean(0));
+console.log(Boolean("Hello"));
+console.log(Boolean(null));
+console.log(Boolean([]));
