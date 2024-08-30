89500ddc6125c0d93390a12191ace85e9fd187ba
chiranjib09 <chiranjibdas28094@gmail.com>
2024-08-30 16:27:02 +0530

    LogicalOperators file added

---
+++ Javascript Basics/Boolean DataType/LogicalOperators.js
@@ -0,0 +1,13 @@
+//  Logical operators allow you to combine or negate Boolean expressions.
+
+//  && (AND): Returns true if both operands are true.
+//  || (OR): Returns true if either operand is true.
+//   (NOT): Inverts the Boolean value.
+
+let x = true;
+let y = false;
+
+console.log(x && y);
+console.log(x || y);
+console.log(!x);
+console.log(!y);
