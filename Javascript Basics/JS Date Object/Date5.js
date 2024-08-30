cecdeec7ad76db8e6bb03461e2ca5dbee9de721c
chiranjib09 <chiranjibdas28094@gmail.com>
2024-08-30 15:37:55 +0530

    Date 5 file added

---
+++ Javascript Basics/JS Date Object/Date5.js
@@ -0,0 +1,9 @@
+// Get Time Components
+
+let time = new Date();
+
+let hours = time.getHours();
+let minutes = time.getMinutes();
+let seconds = time.getSeconds();
+
+console.log(`Time: ${hours}:${minutes}:${seconds}`);
