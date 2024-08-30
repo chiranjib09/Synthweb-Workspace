193fd9471bfa67bf8adba051277175e208f2a4b3
chiranjib09 <chiranjibdas28094@gmail.com>
2024-08-30 16:24:33 +0530

    BooleanExpression file added

---
+++ Javascript Basics/Boolean DataType/BooleanExpression.js
@@ -0,0 +1,13 @@
+//Expressions in JavaScript can evaluate to true or false using comparison operators.
+
+//Comparison Operators:
+//== (loose equality): Compares values for equality, performing type conversion if necessary.
+//=== (strict equality): Compares values for equality without type conversion.
+//  != (loose inequality): Checks if values are not equal, allowing type conversion.
+//  !== (strict inequality): Checks if values are not equal without type conversion.
+// >, >=, <, <=: Greater than, less than, etc.
+
+console.log(5 == '5');
+console.log(5 === '5');
+console.log(10 > 3);
+console.log(5 !== 5);
